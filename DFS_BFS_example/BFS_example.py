d797c6ebb6ac6a6aec2470e128e711cc384e4b30
moongligh <moongligh@gmail.com>
2022-02-11 13:47:08 +0000

    learn_to_coding_test 2022/02/11

---
+++ DFS_BFS_example/BFS_example.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+#BFS 메서드 정의
+def bfs(graph, start, visited):
+  queue = deque([start])
+  visited[start] = True
+
+  while queue:
+    v = queue.popleft()
+    print(v, end=' ')
+
+    for i in graph[v]:
+      if not visited[i]:
+        queue.append(i)
+        visited[i] = True
+
+#각 노드가 연결된 정보를 2차원 리스트로 표현
+graph = [
+  [],
+  [2, 3, 8],
+  [1, 7],
+  [1, 4, 5],
+  [3, 5],
+  [3, 4],
+  [7],
+  [2, 6, 8],
+  [1, 7]
+]
+
+#각 노드가 방문된 정보를 표현하는 리스트
+visited = [False] * 9
+
+bfs(graph, 1, visited)
