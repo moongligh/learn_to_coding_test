8fbb21045a0f0016bcedbc5c758a7259a920244b
moongligh <moongligh@gmail.com>
2022-02-14 15:30:11 +0000

    learn_to_codint_test 2022/02/14 modify

--- DFS_BFS_example/DFS_BFS_example_2.py
+++ DFS_BFS_example/DFS_BFS_example_2.py
@@ -15,29 +15,37 @@
   
   출력조건: 첫재 줄에 최소 이동 칸의 개수를 출력합니다.
 '''
+from collections import deque
 
-def bfs(x, y, count):
-  if x <= -1 or x >= N or y <= -1 or y >= M:
-    count -= 1
-    return count
+def bfs(x, y):
+  #큐 구현을 위해 deque 라이브러리를 사용
+  queue = deque()
+  queue.append((x, y))
 
-  if x == N and y == M:
-    return count
-  elif miro[x][y] == 1:
-    miro[x][y] = 0
-    dfs(x+1, y, count)
-    dfs(x, y+1, count)
-    dfs(x-1, y, count)
-    dfs(x, y-1, count)
-    count += 1
-    return count
+  while queue: #큐가 빌 때까지 반복하기
+    x, y = queue.popleft()
+    for i in range(4): #현재 위치에서 4가지 방향으로의 위치 확인
+      nx = x + dx[i]
+      ny = y + dy[i]
 
-N, M = map(int, input().split())
-miro = []
+      if nx < 0 or nx >= N or ny <0 or ny >= M: #미로 찾기 공간을 벗어난 경우 무시하기
+        continue
+      if miro[nx][ny] == 0: #미로의 벽인 경우 무시하기
+        continue
+      if miro[nx][ny] == 1: #해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+        miro[nx][ny] = miro[x][y] + 1
+        queue.append((nx, ny))
+  
+  return miro[N - 1][M - 1]
+
+N, M = map(int, input().split()) #N, M을 공백을 기준으로 구분하여 입력 받기
+miro = [] #2차원 리스트의 맵 정보 입력 받기
 for i in range(N):
   miro.append(list(map(int, input())))
 
-count = 0
-result = dfs(0, 0, count)
+# 이동할 4가지 방향 백터 값을 정의
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-print(result)
+print(bfs(0, 0))
+print(miro)
